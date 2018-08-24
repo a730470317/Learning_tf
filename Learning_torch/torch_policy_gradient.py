995d88b63e6ea67f2665b94e2c16924b909b1301
ziv.lin <730470317@qq.com>
2018-08-25 00:44:52 +0800

    [Release] policy_gradient work.

--- Learning_torch/torch_policy_gradient.py
+++ Learning_torch/torch_policy_gradient.py
@@ -5,82 +5,101 @@ import time
 
 class Policy_net(torch.nn.Module):
     def __init__(self):
+        hidden_size = 10
         super(Policy_net, self).__init__()
-        self.net_1 = torch.nn.Linear(4, 10)
-        self.net_2 = torch.nn.Linear(10, 10)
-        self.net_out = torch.nn.Linear(10, 2)
+        self.net_1 = torch.nn.Linear(4, hidden_size)
+        self.net_2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.net_out = torch.nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         temp = self.net_1(x)
         temp = self.net_2(temp)
         output = self.net_out(temp)
-        output = torch.nn.functional.softmax(output, dim=1)
+        output = torch.nn.functional.sigmoid(output)
+        # output = torch.nn.functional.softmax(output, dim=1)
         return output
 
-def chose_action(action_val):
-    if(action_val[0] > action_val[1]):
-        return  1
-    else:
-        return  0
 
 if __name__ == "__main__":
-    Learning_rate = 0.2
-    Gamma = 0.98
+    Learning_rate = 0.01
+    Gamma = 0.99
     env = gym.make("CartPole-v0")
-
+    env._max_episode_steps = 500
     policy_net = Policy_net()
-    optimizer = torch.optim.SGD(policy_net.parameters(), lr=Learning_rate)
+    optimizer = torch.optim.Adam(policy_net.parameters(), lr=Learning_rate)
     print(policy_net)
+    log_state = []
+    log_action = []
+    log_reward = []
     for times in range(100000):
         state = env.reset()
-        print("----- start %d -----"%(times))
+        # print("----- start %d -----"%(times))
         award_accumulate = 0
-        log_state = []
-        log_action = []
-        log_reward = []
         # Run policy
         while(1):
-            # env.render()
             np_state = np.array(state)
             torch_state = torch.from_numpy(np_state[np.newaxis, :]).float()
-            action_out = policy_net(torch_state).data.numpy()[0,:]
-            action = chose_action(action_out)
-            # print(action_out)
+            net_out = policy_net(torch_state)[0,:]
+            action = torch.distributions.Bernoulli(net_out).sample()
+            action = action.data.numpy().astype(int)[0]
+            # print(action)
             state, reward, done, _ = env.step( action )
+            # env.render()
+
             award_accumulate = award_accumulate + reward
-            log_state.append(state)
+            if (done == True):
+                reward = 0
+            log_state.append(torch_state)
             log_action.append(action)
             log_reward.append(reward)
             # action_out = torch.distributions.Bernoulli(action_out).sample()
             if(done == True):
                 break
             # print(np.array(state), reward)
+        print(times," , sum of reward is = ", award_accumulate)
+        if(times%5 == 0):
+            print("----- update -----")
+            log_size = len(log_state)
+            R_state = log_reward
+            sum_add = 0
+            for step in reversed( range(log_size) ):
+                idx = step
+                if(log_reward[idx] ==0):
+                    sum_add = 0
+                else:
+                    sum_add = Gamma*sum_add + log_reward[idx]
+                R_state[idx] = sum_add
 
-        log_size = len(log_state)
-        R_state = log_reward
-        for step in range(log_size-1):
-            idx=  log_size - 2 - step
-            R_state[idx] = log_reward[idx] + Gamma*R_state[idx+1]
-        np.set_printoptions(precision=2)
-        # print(np.array(R_state))
-        # Update policy
-        print("Sum of reward is = " , award_accumulate )
-        for step in range(log_size):
-            net_output = policy_net(torch_state)[0, :]
-            # print(net_output)
-            if chose_action(net_output.data.numpy()):
-                # loss = torch.nn.MSELoss(net_output, torch.tensor([0,1]))
-                loss = -torch.dot(net_output, torch.tensor([1.0, 0.0]))
-                # loss = - torch.log(idx.float()) / log_size * award_accumulate
-            else:
-                # loss = torch.nn.MSELoss(net_output, torch.tensor([0, 1]))
-                # loss = -torch.dot(net_output, torch.tensor([0.0, 1.0])) / log_size * award_accumulate
-                loss = -torch.dot(net_output, torch.tensor([0.0, 1.0]))
-            loss = loss* R_state[step]
-            # print(loss)
+            # print(np.array(R_state))
+
+            np.set_printoptions(precision=2)
+            reward_mean = np.mean(R_state)
+            reward_std = np.std(R_state)
+            for step in range(log_size):
+                R_state[step] = (R_state[step] - reward_mean)/reward_std
+
+            # print(np.array(R_state))
+
             optimizer.zero_grad()
-            loss.backward()
+            # Update policy
+
+            for step in range(log_size):
+                pass
+                net_out = policy_net(log_state[step])
+                action = torch.autograd.Variable(torch.FloatTensor([log_action[step]]))
+
+                m = torch.distributions.Bernoulli(net_out)
+                # print(net_out, ' ---- ', action)
+                # print(m.log_prob(action))
+
+                loss = -m.log_prob(action) * R_state[step]
+                loss.backward()
+
             optimizer.step()
+
+            log_state = []
+            log_action = []
+            log_reward = []
 
     env.close()
     print("finish")
